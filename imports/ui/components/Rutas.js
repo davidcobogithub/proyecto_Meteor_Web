5d2782beb2c7268f9f3363928a51380a77d91e67
David Cobo <davip.95@hotmail.com>
2018-11-11 00:36:46 -0500

    My commit 2!

--- imports/ui/components/Rutas.js
+++ imports/ui/components/Rutas.js
@@ -8,11 +8,17 @@ import Login from './Login/Login'
 const AppRoutes = () =>
     <Switch>
      
-     <Redirect from="/login" to="/"/>
+     {/* <Redirect from="/login" to="/"/>
       <Route exact path="/"  component={Login}/>
       <Route exact path="/proyectos"  component={Proyecto}/>
       <Route exact path="/proyectoNuevo" component={ProyectoNuevo}/>
-      <Route exact path="/tareas" component={Tarea}/>
-     
+      <Route exact path="/tareas" component={Tarea}/> */}
+
+       <Redirect from="/login" to="/"/>
+      <Route exact path="/"  component={Login}/>
+      <Route exact path="https://tallermeteorproy.herokuapp.com/proyectos"  component={Proyecto}/>
+      <Route exact path="https://tallermeteorproy.herokuapp.com/proyectoNuevo" component={ProyectoNuevo}/>
+      <Route exact path="https://tallermeteorproy.herokuapp.com/tareas" component={Tarea}/>
+    
     </Switch>
 export default AppRoutes;
